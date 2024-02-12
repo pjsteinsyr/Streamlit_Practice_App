f9d65e2e622beefdb7f73c060102e05fe3bf0865
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 10:43:39 -0500

    Update Streamlit_Practice.py

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -2,10 +2,9 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 st.title("Peter's Practice Site")
 data = pd.read_csv("data.csv")
-st.write(data.head())
-fig, ax = plt.subplots()
-ax.hist(data.Height, bins = 10)
-st.pyplot(fig)
+
