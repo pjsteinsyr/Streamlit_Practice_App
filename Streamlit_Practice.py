8e43214b77aae2169eaa6729213decbf349c592c
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 13:47:14 -0500

    u

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -46,4 +46,4 @@ st.write(f'Mean squared Error = {metrics.mean_squared_error(y_test, lin_pred)}')
 st.header('Try it for yourself!')
 number = st.number_input('Insert a number')
 #your_prediction = lin_reg.predict(number)[0]
-print(number)
+st.write(number)
