88e41aac642bb18ffdb773c6121a46d0143db89a
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 14:43:48 -0500

    u

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -45,7 +45,7 @@ st.write(f'Mean squared Error = {metrics.mean_squared_error(y_test, lin_pred)}')
 
 st.header('Try it for yourself!')
 number = st.number_input('Insert your weight (kgs)')
-ur_number = pd.DataFrame(data = {"Height" : [number]})
+ur_number = pd.DataFrame(data = {"Weight" : [number]})
 if st.button("Print Height"):
     # Display the entered number when the button is clicked
     st.write(f"Your estimated height is {lin_reg.predict(ur_number)[0]}")
