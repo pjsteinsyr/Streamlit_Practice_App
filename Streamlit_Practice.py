3ce91cc5e1af5d852f39604172e8dd571d16b896
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 13:06:41 -0500

    u

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -41,7 +41,7 @@ results = pd.DataFrame(data = {"pred": lin_pred, "act": y_test})
 results['hgt'] = x_test
 st.write(results)
 st.write(f'R square = {metrics.r2_score(y_test, lin_pred)}')
-#st.write('Mean squared Error = ',metrics.mean_squared_error(y_test, lin_pred))
+st.write(f'Mean squared Error = {metrics.mean_squared_error(y_test, lin_pred)}')
 
 st.header('Try it for yourself!')
 
