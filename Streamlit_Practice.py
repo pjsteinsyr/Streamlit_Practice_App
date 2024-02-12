14848126f2f889a83d82489fde21fcce9bab69db
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 11:04:45 -0500

    update

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -15,7 +15,7 @@ x = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
 train = pd.DataFrame(data= {'height': x_train, 'weight': y_train})
-st.write()
+st.write(train)
 lin_reg = LinearRegression()
 lin_reg.fit(x_train, y_train)
 
