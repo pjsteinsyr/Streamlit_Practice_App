de28f1eb35261e56e71493dd49f120c296708d96
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 11:16:50 -0500

    u

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -16,6 +16,11 @@ y = data.iloc[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
 train = pd.merge(x_train, y_train,  left_index=True, right_index=True)
 st.write(train.head())
+
+fig, ax = plt.subplots()
+ax.plot(train{'Height'}, train['Weight'])
+st.pyplot(fig)
+
 lin_reg = LinearRegression()
 lin_reg.fit(x_train, y_train)
 
