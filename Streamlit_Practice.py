4530b8a106513275ebdd4f39fcf9e42070dc8448
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 10:41:57 -0500

    Update Streamlit_Practice.py

--- Streamlit_Practice.py
+++ Streamlit_Practice.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 st.title("Peter's Practice Site")
 data = pd.read_csv("data.csv")
+st.write(data.head())
 fig, ax = plt.subplots()
 ax.hist(data.Height, bins = 10)
 st.pyplot(fig)
